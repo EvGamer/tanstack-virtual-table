d54651d12cf04cb64df43789721c6f0d361bbb1d
Eugene Kuzmenko <e.kuzmenko@dunice.net>
2026-07-23 17:56:33 +0300

    fix: typescript mismatch by using manual webpack config

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+import path from 'path'
+import { fileURLToPath } from 'url'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url))
+
+/** @type import('webpack').Configuration */
+export default ({ WEBPACK_SERVE }) => ({
+  target: 'web',
+  mode: WEBPACK_SERVE ? 'development' : 'production',
+  entry: path.resolve(__dirname, './src/index.tsx'),
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].bundle.js',
+    publicPath: '/',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './public/index.html'),
+      filename: 'index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /(node_modules)/,
+        use: { loader: 'swc-loader' },
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: {
+                namedExport: false,
+                auto: true,
+              }
+            }
+          }
+        ],
+      },
+    ],
+  },
+  devServer: {
+    open: true,
+    hot: true,
+    historyApiFallback: {
+      rewrites: [{ from: /./, to: '/index.html' }],
+    },
+    static: ['public'],
+  },
+})
